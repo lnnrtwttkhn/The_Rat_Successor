7fa228b7c743ff89543d3623e6d2fd8edf5bc0bb
cofficer <christoffer.gahnstrom@hotmail.com>
2019-08-10 18:07:28 -0400

    place field

---
+++ matlab/place_field.m
@@ -0,0 +1,42 @@
+function place_field 
+
+addpath(genpath('/Users/Christoffer/Dropbox/PhD/Projects/the_rat_successor/code'))
+load('/Users/Christoffer/Dropbox/PhD/Projects/the_rat_successor/The_Rat_Successor/DataFiles/AJF016/CD3/AJF016CD3SpksEvs.mat')
+
+spkmat
+
+x1 = unitdata.rawLEDs(:,2);
+y1 = unitdata.rawLEDs(:,3);
+x2 = unitdata.rawLEDs(:,4);
+y2 = unitdata.rawLEDs(:,5);
+
+led_ts=unitdata.rawLEDs(:,1);
+
+% Match time stamps of the ephys. 
+eventsTimestamps=load_ephys(); 
+
+eventsTimestamps.all_ts(1:10)
+
+spike_ts = unitdata.units(1).ts;
+
+
+spike_ts(1:10)
+led_ts(1:10)
+
+spkx = interp1(led_ts,[x1 y1],spike_ts,'nearest');
+
+% norm by occupancy
+% 
+
+
+
+
+for ispike=1:spike_ts
+if spike_ts(ispike)<led_ts (spike_ts(ispike)<X) && (X<1)
+    
+end
+end 
+
+plot(x1,y1)
+
+plot(unitdata.units(1).ts)
